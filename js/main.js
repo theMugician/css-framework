cf40fb5ccc7ceb675e8a827084d4487f218a6202
theMugician <gslonina7@gmail.com>
2021-04-13 12:31:06 -0500

    Modal toggle functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 //Write your script here
 
 // for IE10, .getElementsByClassName()[0];
-const toggleMobileMenu = document.querySelector(".js-toggle-mobile-menu")
-const mobileMenu = document.querySelector(".nav__mobile-menu")
+/*
+const toggleMobileMenu = document.querySelector('.js-toggle-mobile-menu')
+const mobileMenu = document.querySelector('.nav__mobile-menu')
 
 
-toggleMobileMenu.addEventListener('click', function() {
-  console.log("toggle clicked")
-  mobileMenu.classList.toggle("active")
-}, 
-false)
+toggleMobileMenu.addEventListener('click', function () {
+  console.log('toggle clicked')
+  mobileMenu.classList.toggle('active')
+}, false)
+*/
+const modal = document.querySelector('.modal')
 
-const openModal = document.querySelector(".js-open-modal")
-const closeModal = document.querySelector(".js-close-modal")
-const modal = document.querySelector(".modal")
+const toggleModal = document.querySelector('.js-toggle-modal')
+
+/*
+const openModal = document.querySelector('.js-open-modal')
+const closeModal = document.querySelector('.js-close-modal')
+
+[ openModal, closeModal ].forEach(function (element) {
+   element.addEventListener('click', function () {
+    console.log('modal clicked')
+  	modal.classList.toggle('active')
+   })
+})
+*/
+
+toggleModal.addEventListener('click', function () {
+	let targetModalId = this.getAttribute('href')
+	targetModalId = targetModalId.slice(1,targetModalId.length)
+  let targetModal = document.getElementById(targetModalId)
+  console.log(targetModal)
+  
+  targetModal.classList.toggle('active')
+}, false)
