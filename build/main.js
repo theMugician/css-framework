0ce501a61a8b8100486a2c7dcf9dbd342bf46b97
theMugician <gslonina7@gmail.com>
2021-04-12 23:02:49 -0500

    Mobile menu, grid system and basic docs

--- build/main.js
+++ build/main.js
@@ -1,1 +1,12 @@
 //Write your script here
+
+// for IE10, .getElementsByClassName()[0];
+var toggleMobileMenu = document.querySelector(".js-toggle-mobile-menu")
+var mobileMenu = document.querySelector(".nav__mobile-menu")
+
+
+toggleMobileMenu.addEventListener('click', function() {
+  console.log("toggle clicked")
+  mobileMenu.classList.toggle("active")
+}, 
+false)
