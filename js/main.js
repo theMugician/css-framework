ca376e1f9061f17d18d6ede4ade1ad7f9cdfba07
theMugician <gslonina7@gmail.com>
2021-04-15 22:16:25 -0500

    Fix dropdown issues

--- js/main.js
+++ js/main.js
@@ -1,37 +1,16 @@
-//Write your script here
 
-// for IE10, .getElementsByClassName()[0];
-/*
-const toggleMobileMenu = document.querySelector('.js-toggle-mobile-menu')
-const mobileMenu = document.querySelector('.nav__mobile-menu')
-
-
-toggleMobileMenu.addEventListener('click', function () {
-  console.log('toggle clicked')
-  mobileMenu.classList.toggle('active')
-}, false)
-*/
 const modal = document.querySelector('.modal')
 
-const toggleModal = document.querySelector('.js-toggle-modal')
+const toggleModals = document.querySelectorAll('.js-toggle-modal')
 
-/*
-const openModal = document.querySelector('.js-open-modal')
-const closeModal = document.querySelector('.js-close-modal')
-
-[ openModal, closeModal ].forEach(function (element) {
-   element.addEventListener('click', function () {
-    console.log('modal clicked')
-  	modal.classList.toggle('active')
-   })
+toggleModals.forEach(toggle => { 
+  toggle.addEventListener('click', function () {
+  	let targetModalId = this.getAttribute('href')
+  	targetModalId = targetModalId.slice(1,targetModalId.length)
+    let targetModal = document.getElementById(targetModalId)
+    console.log('Toggle click')
+    
+    targetModal.classList.toggle('active')
+  }, false)
 })
-*/
 
-toggleModal.addEventListener('click', function () {
-	let targetModalId = this.getAttribute('href')
-	targetModalId = targetModalId.slice(1,targetModalId.length)
-  let targetModal = document.getElementById(targetModalId)
-  console.log(targetModal)
-  
-  targetModal.classList.toggle('active')
-}, false)
